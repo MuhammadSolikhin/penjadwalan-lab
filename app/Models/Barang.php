7618c31aa0f122d561535051881045149e5e5459
Yunsavara <yunikosatria23@gmail.com>
2025-07-02 14:01:00 +0700

    WIP: Barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    //
+    protected $fillable = [
+        'nama',
+        'spesifikasi',
+        'deskripsi',
+        'status',
+        'kategori_barang_id',
+        'lab_id',
+        'meja_id',
+    ];
+
+    public function kategoriBarang()
+    {
+        return $this->belongsTo(KategoriBarang::class, 'kategori_barang_id');
+    }
 }
---
+++ resources/views/livewire/barang/barang.blade.php
@@ -0,0 +1,12 @@
+<div>
+    @section('title', 'Barang')
+
+    @section('content')
+        <h2 class="fw-bold fs-3">Barang</h2>
+        <span>Halaman untuk mengelola barang</span>
+        <hr>
+        <div id="container-fluid">
+
+        </div>
+    @endsection
+</div>
