0a8c048b2ba6c8b623c6139dce8b2b8d73563ab2
abihanafi23 <hanafi.abi50@gmail.com>
2025-07-18 10:40:14 +0700

    ROUTES BUG FIX

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::group(['middleware' => 'guest'], function () {
 
 Route::group(['middleware' => 'auth'], function () {
     // Profile Page
-    Route::get('/profile/{id}', [UsersController::class, 'show'])->name('profile.show');
+    Route::get('/profile/{id}', [UsersController::class, 'show'])->name('profile.show'); 
 
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
