40c5bfa474707086e1f2a07152ca432f71b25c11
Muhammad Solikhin <school.muhammad.solikhin@gmail.com>
2025-08-16 23:19:13 +0700

    Refactor Docker setup and improve environment configuration
    - Updated Dockerfile to use alpine images for PHP and Nginx for a smaller footprint.
    - Enhanced docker-compose.yml for better environment variable management and volume handling.
    - Cleaned up .dockerignore to streamline ignored files.
    - Removed unnecessary comments and improved readability in configuration files.
    - Minor UI adjustment in kategori-barang.blade.php by removing debug output.

--- resources/views/livewire/kategori-barang/kategori-barang.blade.php
+++ resources/views/livewire/kategori-barang/kategori-barang.blade.php
@@ -5,7 +5,6 @@
 
         <h2 class="fw-bold fs-3">Kategori Barang</h2>
         <span>Halaman untuk mengelola kategori barang</span>
-         <pre>{{ Route::currentRouteName() }}</pre> 
         <hr>
         <div id="container-fluid">
             <livewire:kategoribarang.kategori-barang-table :currentRoute="$path" />
